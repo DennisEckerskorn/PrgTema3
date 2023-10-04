b4969f2b3dc903152259be0060fe04b3dbc51bd7
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-04 15:40:41 +0200

    Update

--- EjerciciosTemaTres/Ejercicio5.java
+++ EjerciciosTemaTres/Ejercicio5.java
@@ -9,27 +9,18 @@ public class Ejercicio5{
 		numeroUsuario = Integer.parseInt(lector.nextLine());
 		//Switch para elegir el caso apropiado al número.
 		switch(numeroUsuario) {
-			case 0: System.out.println("INSUFICIENTE");
-				break;
-			case 1: System.out.println("INSUFICIENTE");
-				break;
-			case 2: System.out.println("INSUFICIENTE");
-				break;
-			case 3: System.out.println("INSUFICIENTE");
-				break;
-			case 4: System.out.println("INSUFICIENTE");
+			case 0: case 1: case 2: case 3: case 4:
+				System.out.println("INSUFICIENTE");
 				break;
 			case 5: System.out.println("SUFICIENTE");
 				break;
 			case 6: System.out.println("BIEN");
 				break;
-			case 7: System.out.println("NOTABLE");
+			case 7: case 8: 
+				System.out.println("NOTABLE");
 				break;
-			case 8: System.out.println("NOTABLE");
-				break;
-			case 9: System.out.println("SOBRESALIENTE");
-				break;
-			case 10: System.out.println("SOBRESALIENTE");
+			case 9: case 10:
+				System.out.println("SOBRESALIENTE");
 				break;
 			default:
 				System.out.println("ERROR");
