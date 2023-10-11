53cf3b64b7cb1eac517d2d919d1cc34f9806e0ce
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-11 15:37:48 +0200

    update

--- EjerciciosTemaTres/Bucles/Ejercicio10.java
+++ EjerciciosTemaTres/Bucles/Ejercicio10.java
@@ -3,17 +3,17 @@ public class Ejercicio10{
 	public static void main(String[] args){
 		//Variables.
 		Scanner lector = new Scanner(System.in);
-		int numUsuario;
-		int suma = 0;
+		double numUsuario;
+		double suma = 0;
 		//Bucle do while, pedimos numeros mientras el numero no es 0. Cada iteracion suma otro numero.
 		do {
 			System.out.println("Ingresa un número");
-			numUsuario = Integer.parseInt(lector.nextLine());
+			numUsuario = Double.parseDouble(lector.nextLine());
 			suma = suma + numUsuario;
 		}while(numUsuario != 0);
-		//Mostramos el resultado.
+		//Se muestra el resultado.
 		System.out.println("La suma de los numeros ingresados es: " + suma);
-		
+		//Se cierra el Scanner.
 		lector.close();
 	}
 }
--- EjerciciosTemaTres/Bucles/Ejercicio12.java
+++ EjerciciosTemaTres/Bucles/Ejercicio12.java
@@ -3,39 +3,40 @@ import java.lang.Math;
 
 public class Ejercicio12{
 	public static void main(String[] args){
+		//Declaración de las variables.
 		Scanner lector = new Scanner(System.in);
 		int numeroAleatorio = (int) (Math.random()*100)+1; //Se genera un numero aleatorio entre 1 y 100, se hace un cast porque random devuelve un double pero queremos un numero entero.
 		int numeroUsuario;
 		int numeroIntentos = 5;
 	
-
+		//Bucle do while para pedir números mientras se cumple una función.
 		do{	
 			//Prueba para ver el número aleatorio: System.out.println(numeroAleatorio);
 			System.out.println("***JUEGO DE ADIVINAR EL NÚMERO***");
 			System.out.println("Tienes " + numeroIntentos + " intentos para adivinar un número entre 1 y 100, empecemos:");
 			System.out.println("Ingresa el primer número:");
 			numeroUsuario = Integer.parseInt(lector.nextLine());
-			
+			//Si el numero es mayor.
 			if(numeroAleatorio > numeroUsuario){
 				System.out.println("El número aleatorio es mayor, intenta otra vez");
 				numeroIntentos = numeroIntentos - 1;
-
+			//Si el numero es menor.
 			}else if (numeroAleatorio < numeroUsuario){
 				System.out.println("El número aleatorio es menor, intenta otra vez");
 				numeroIntentos = numeroIntentos - 1;
-
+			//Si el numero es igual.
 			}else if (numeroAleatorio == numeroUsuario){
 				System.out.println("HAS ACERTADO, el número es igual");
 				numeroIntentos = numeroIntentos - 1;
 				System.out.println("HAS GANADO CON ");
 				break;
 			}
-			
+			//Si se acaban los intentos imprimios un mensaje.
 			if (numeroIntentos == 0){
 				System.out.println("Has llegado al maximo de intentos, vuelve a ejecutar el programa para jugar de nuevo");		
 			}
 		}while(numeroIntentos != 0);
-
+		//Se cierra el Scanner.
 		lector.close();
 	}
 }
