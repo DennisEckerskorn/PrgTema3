8a2ea3162b77bf33aaf8750216888278da0367c0
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-18 12:46:07 +0200

    added comments

--- EjerciciosTemaTres/Bucles/Ejercicio16.java
+++ EjerciciosTemaTres/Bucles/Ejercicio16.java
@@ -27,17 +27,22 @@ public class Ejercicio16{
 			puntosJugador += dado;
 			System.out.println("El jugador ha tirado un " + dado + " y tiene " + puntosJugador + " puntos.");
 
-			//a)
+			//a) Obtenga exactamente 11 puntos (ha ganado). En el caso a) sumará automáticamente dos porras (1 porra por victoria y otra de bonificación por alcanzar la puntuación máxima).
 			if(puntosJugador == 11){
 				porraJugador += 2;
 				System.out.println("Has ganado 11 puntos y obtienes 2 porras.");
 				System.out.println("Puntuación: " + porraJugador + " porras.");
-			//c)
+
+			//c) Se haya pasado de 11 puntos y por tanto habrá perdido. En el caso c) el ordenador gana automáticamente y suma una porra sin necesidad de jugar. El primero que llegue a 5 porras gana la partida.
 			} else if (puntosJugador > 11) {
 				porraOrdenador += 1;
 				System.out.println("Te has pasado de 11. El ordenador recibe 1 porra.");
 				puntosJugador = 0;
-			//b)	
+
+			//b) Decida plantarse con una puntuación inferior a 11 puntos. En el caso b) el ordenador jugará e intentará igualar o superar sin pasarse de 11 puntos la
+			// puntuación del jugador. Si consigue igualar o superar la puntuación sumará una porra para el
+			// ordenador. Si además lo hace con una puntuación de 11 puntos tendrá bonificación de otra porra
+			// por alcanzar la puntuación máxima.
 			} else {
 				System.out.println("¿Quieres plantarte o seguir? [S - Seguir] o [P - Plantarte]");
 				opcion = lector.next().charAt(0);
@@ -49,7 +54,7 @@ public class Ejercicio16{
 						puntosOrdenador += dado;
 					}
 				
-					if (puntosOrdenador == puntosJugador) {
+					if (puntosOrdenador >= puntosJugador) {
 						porraOrdenador += 1;
 						System.out.println("El ordenador iguala tus puntos y gana 1 porra.");
 					} else if (puntosOrdenador == 11) {
@@ -68,5 +73,7 @@ public class Ejercicio16{
 		} else {
 			System.out.println("El ordenador ha ganado con " + porraOrdenador + " porras.");
 		}
+		
+		lector.close();
 	}
 }
