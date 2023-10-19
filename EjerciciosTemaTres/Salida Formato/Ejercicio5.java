aad4780ff15754147206724d46d0ece1ed8304d1
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-19 11:30:27 +0200

    upd

--- EjerciciosTemaTres/Salida Formato/Ejercicio5.java
+++ EjerciciosTemaTres/Salida Formato/Ejercicio5.java
@@ -7,8 +7,8 @@ public class Ejercicio5 {
                 		System.out.print(" ");
                 	} else {
 				int color = 30 + i - 1; 				//El numero 30 va junto a i, aumenta cuando aumenta i.
-				if(color > 37){						//Si el color es mayor que 37 se reinicia la variable para volver a empezar desde 30.
-				 color = 30;
+				if(color > 37){						//Si el color es mayor que 37 se reinicia la variable para volver a empezar desde 30. Solo sirve en este caso.
+					color = 30;
 				}
                 		System.out.printf("\u001B[%dm%d",color, j - (9 - i));	// Usamos %d para imprimir el número entero y se usa 9-1 para imprimir los desplazamientos vacios, Ejemplo: i es 1, se imprimen 8 espacios.
                 	}
