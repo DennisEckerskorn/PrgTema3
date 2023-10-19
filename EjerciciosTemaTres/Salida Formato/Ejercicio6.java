e9d1e4a044c41002dda56db886bafa9d77ef0c03
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-19 14:05:40 +0200

    cr

---
+++ EjerciciosTemaTres/Salida Formato/Ejercicio6.java
@@ -0,0 +1,10 @@
+public class Ejercicio6{
+	public static void main(String[] args){
+		int contador = 0;	
+		for (int i = 1; i <= 9; i++){
+			contador++;
+			System.out.println(i + " " + contador);
+		}
+		
+	}
+}
