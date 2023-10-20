0bf09d674b06cf4b57fc65e865f18ee188301d91
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-10-20 14:21:11 +0200

    Update Ejercicio7.java

--- EjerciciosTemaTres/SalidaFormato/Ejercicio7.java
+++ EjerciciosTemaTres/SalidaFormato/Ejercicio7.java
@@ -3,23 +3,15 @@ public class Ejercicio7{
 		String numerosI = "";
 		String numerosInvertidos = "";
 		
-			//Se recorre el bucle del 1 al 9.		
-			for (int i = 1; i < 9; i++){
-				numerosI += i;
-				numerosInvertidos = numerosI;
-				//Recorre los numeros de 1 al 9 y los concatena al reves.
-				for (int j = numerosI.length() - 1; j >= 0; j--){
-					
-					numerosInvertidos += numerosInvertidos.charAt(j);
-					
-					
-					
-				} 
-				//System.out.printf("%9s\n", numerosI, numerosInvertidos);
-				System.out.printf("\u001B[7m%9s\u001B[0m\n", numerosInvertidos);
-			}
-			
-		
-						
+		//Se recorre el bucle del 1 al 9.		
+		for (int i = 1; i <= 9; i++){
+			numerosI += i;
+			numerosInvertidos = numerosI;
+			//Recorre los numeros de 1 al 9 y los concatena al reves.
+			for (int j = numerosI.length() - 1; j >= 0; j--){
+				numerosInvertidos += numerosInvertidos.charAt(j);	
+			} 
+			System.out.printf("\u001B[7m%9s%-9s\u001B[0m\n", numerosI, numerosInvertidos);
+		}	
 	}
 }
